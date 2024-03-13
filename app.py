4c6d6816b8f3a8930a7778aaf45cd8ad7f20a53e
Gianni de Nobile <gianni.denobile@hotmail.com>
2024-03-13 23:32:50 +0100

    "commit"

--- app.py
+++ app.py
@@ -7,22 +7,6 @@ app = Flask(__name__, static_url_path='/', static_folder='web')
 def indexPage():
      return send_file("web/index.html")  
 
-from markupsafe import escape
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
 @app.route("/sum")
 def sum_even():
     # flask parameters with type and default
