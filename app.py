ff73a9c0d7b7d03f8e85d14ae65be6df510ea393
Gianni de Nobile <gianni.denobile@hotmail.com>
2024-03-16 19:19:05 +0100

    changes made

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, jsonify
-from flask.helpers import send_file
+from flask import Flask, request, send_from_directory, jsonify
 
-app = Flask(__name__, static_url_path='/', static_folder='web')
+app = Flask(__name__, static_folder='web')
 
 @app.route("/")
-def indexPage():
-     return send_file("web/index.html")  
+def index_page():
+    return send_from_directory('web', 'index.html')
 
-@app.route("/sum")
-def sum_even():
-    # flask parameters with type and default
-    n = request.args.get('n', default=1, type=int)
-    # logic
-    result = sum([x for x in range(n+1) if x % 2 == 0])
-    # return result as json
-    return jsonify(sum=result)
+@app.route('/calc', methods=['POST'])
+def calc():
+    input = request.get_json()
+    result = 0
+    if 'number1' in input and 'number2' in input and 'operation' in input:
+        number1 = float(input['number1'])
+        number2 = float(input['number2'])
+        operation = input['operation']
+        
+        if operation == 'add':
+            result = number1+number2
+        elif operation == 'sub':
+            result = number1-number2
+        else:
+            return jsonify({'error': 'You can only add or subtract'}), 400
+        return jsonify({'result': result})
+    else:
+        return jsonify({'error': 'Input two numbers and an operation'}), 400
--- web/script.js
+++ web/script.js
@@ -1,13 +1,27 @@
-function inputChanged(event) {
-    fetch('/sum?n=' + event.target.value )
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            document.getElementById("result").value = data["sum"];
-        });
+function calc(operation) {
+    event.preventDefault()
+    console.log("calc() function called with operation:", operation); // Add this line
+    var number1 = parseFloat(document.getElementById('number1').value);
+    var number2 = parseFloat(document.getElementById('number2').value);
+
+    var requestBody = {
+        number1: number1,
+        number2: number2,
+        operation: operation
+    };
+
+    fetch('/calc', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+    })
+    .then(response => response.json())
+    .then(input => {
+        document.getElementById('result').innerText = "Result: " + input.result;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
-
-
-
-
-
